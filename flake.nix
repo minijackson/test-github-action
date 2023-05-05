4ca8d2981c394b5d531b440c074aa14f599fa0d3
Minijackson <minijackson@riseup.net>
2023-05-05 17:03:41 +0200

    add book

--- flake.nix
+++ flake.nix
@@ -6,8 +6,35 @@
     self,
     nixpkgs,
   }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.book = with nixpkgs.legacyPackages.x86_64-linux;
+      stdenvNoCC.mkDerivation {
+        name = "book";
+        src = ./doc;
+
+        nativeBuildInputs = [quarto];
+
+        dontConfigure = true;
+
+        buildPhase = ''
+          runHook preBuild
+
+          export HOME=$PWD
+          quarto render
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          cp -r _site/ "$out"
+
+          runHook postInstall
+        '';
+      };
 
     checks.x86_64-linux.all-packages = self.packages.x86_64-linux.hello;
 
